2bbcf4e9fb64b4956f9a0261c47d1a543d1bebe0
Alice Caorali <alice.caporali@hotmail.fr>
2017-12-26 18:56:52 +0100

    Correction - Visionneuse d images de nouveau fonctionnelle

--- site/web/js/567b439.js
+++ site/web/js/567b439.js
@@ -272,15 +272,15 @@ $(".element-down").inViewport(function(px){
   }
 });
 
-$(document).on('click','.galerie-photos .photos ul li a img', function(e) {
+$(document).on('click','.galerie-photos .photos div.photo a img', function(e) {
   e.preventDefault();
   var imageSrc = $(this).attr("src");
-  $('.galerie-photos .photos ul li').removeClass("show");
-  $(this).closest('.galerie-photos .photos ul li').addClass("show");
+  $('.galerie-photos .photos div.photo').removeClass("show");
+  $(this).closest('.galerie-photos .photos div.photo').addClass("show");
 
   if ($('#lightbox').length > 0) { // #lightbox exists
     //insert img tag with clicked link's href as src value
-    $('#lightbox .contenu').html('<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span><img style="max-height: '+ ($(window).height()-100) +'px;" src="' + imageSrc + '" /><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>');
+    $('#lightbox .contenu').html('<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span><img style="max-height: '+ ($(window).height()-100) +'px; max-width: '+ ($(window).width()-130) +'px;" src="' + imageSrc + '" /><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>');
     $('#lightbox').show();
   }else { //#lightbox does not exist
     //create HTML markup for lightbox window
@@ -289,7 +289,7 @@ $(document).on('click','.galerie-photos .photos ul li a img', function(e) {
       '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></div>' +
       '<div class="contenu col-xs-12 col-sm-10 col-sm-offset-1">' + //insert clicked link's href into img src
         '<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>'+
-        '<img style="max-height: '+ ($(window).height()-100) +'px;" src="' + imageSrc+'" />' +
+        '<img style="max-height: '+ ($(window).height()-100) +'px; max-width: '+ ($(window).width()-130) +'px;" src="' + imageSrc+'" />' +
         '<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>'+
       '</div>' +
     '</div>';
@@ -310,15 +310,15 @@ $(document).on('click', '#lightbox span.glyphicon-remove-circle', function(){
 function boucleVisionneuse(sens) {
   /*Récupère le src de l'img du li suivant et le met dans le lightbox*/
   if (sens == "prev") {
-    var PrecSuivli = $('.galerie-photos .photos ul li.show').prev('li');
+    var PrecSuivli = $('.galerie-photos .photos div.show').prev('div.photo');
   }else{
-    var PrecSuivli = $('.galerie-photos .photos ul li.show').next('li');
+    var PrecSuivli = $('.galerie-photos .photos div.show').next('div.photo');
   }
   var imgPrecSuivli = PrecSuivli.find('img');
   var imgSrc = imgPrecSuivli.attr("src");
 
   if (imgSrc != null) {
-    $('.galerie-photos .photos ul li').removeClass("show");
+    $('.galerie-photos .photos div.photo').removeClass("show");
     PrecSuivli.addClass("show");
 
     $('#lightbox .contenu').html('<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span><img src="' + imgSrc + '" /><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>');
@@ -326,14 +326,14 @@ function boucleVisionneuse(sens) {
   }else{
     /*Boucle : retour à la première image*/
     if (sens == "prev") {
-      var premierDernierLi = $('.galerie-photos .photos ul li').last();
+      var premierDernierLi = $('.galerie-photos .photos div.photo').last();
     }else{
-      var premierDernierLi = $('.galerie-photos .photos ul li').first();
+      var premierDernierLi = $('.galerie-photos .photos div.photo').first();
     }
     var imgPremierDernierLi = premierDernierLi.find('img');
     var imgSrcPremierDernierLi = imgPremierDernierLi.attr("src");
 
-    $('.galerie-photos .photos ul li').removeClass("show");
+    $('.galerie-photos .photos div.photo').removeClass("show");
     premierDernierLi.addClass("show");
 
     $('#lightbox .contenu').html('<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span><img src="' + imgSrcPremierDernierLi + '" /><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>');
@@ -342,6 +342,7 @@ function boucleVisionneuse(sens) {
 
   //Image dans viewport du navigateur
   $('#lightbox img').css('max-height', $(window).height()-100);
+  $('#lightbox img').css('max-width', $(window).width()-130);
   console.log('lu');
 }
 
